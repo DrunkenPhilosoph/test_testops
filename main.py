3a4a324203683c19f651196f915f9ea531c5f3f1
DrunkenPhilosoph <130548981+DrunkenPhilosoph@users.noreply.github.com>
2023-04-13 16:51:17 +0300

    create_test

--- main.py
+++ main.py
@@ -1,16 +1,11 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import allure
+import pytest
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+@allure.story('Тестирование метода добавления отпуска с позитивными данными')
+@pytest.mark.parametrize('comment', [(1)])
+def test_number(comment):
+    item = comment
+    with allure.step(f'Вводные данные: {comment}'):
+        pass
+    assert comment == 1
