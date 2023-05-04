0dcc750b116a821a7621e6ff58edede42ea1efdf
DrunkenPhilosoph <130548981+DrunkenPhilosoph@users.noreply.github.com>
2023-05-04 15:31:48 +0300

    csdfg

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import pytest
 
 
 @allure.story('Тестирование метода сравнения чисел')
-@pytest.mark.parametrize('comment', [(1)])
+@pytest.mark.parametrize('comment', [('1')])
 def test_number(comment):
     item = comment
     with allure.step(f'Вводные данные: {comment}'):
