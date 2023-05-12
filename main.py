1e2cc21c303a46fad6c01c49eb28871d60962fb7
DrunkenPhilosoph <130548981+DrunkenPhilosoph@users.noreply.github.com>
2023-05-12 11:59:51 +0300

    Update main.py
    123

--- main.py
+++ main.py
@@ -1,12 +1,40 @@
-import allure
 import pytest
+import allure
+
+@allure.feature("Моя фича")
+@allure.story("Мой сценарий")
+@allure.title("Мой тест")
+def test_example():
+    # Шаги теста с декораторами allure
+    with allure.step("Шаг 1: Подготовка данных"):
+        # Подготовка данных для теста
+        data = 'test'
+
+    with allure.step("Шаг 2: Выполнение действия"):
+        # Выполнение действия
+        result = 'test'
+
+    with allure.step("Шаг 3: Проверка результата"):
+        # Проверка результата
+        assert result == data
+
+# Запуск тестов с использованием Allure и сохранением отчета
+if __name__ == "__main__":
+    # Параметры командной строки Allure
+    args = [
+        "--alluredir", "allure-results",  # Путь для сохранения результатов
+        "--clean-alluredir"  # Очистка предыдущих результатов
+    ]
+
+# import allure
+# import pytest
 
 
-@allure.story('Тестирование метода сравнения чисел')
-@pytest.mark.parametrize('comment', [('1')])
-def test_number(comment):
-    item = comment
-    with allure.step(f'Вводные данные: {comment}'):
-        pass
-    assert comment == '1'
-    print(comment)
+# @allure.story('Тестирование метода сравнения чисел')
+# @pytest.mark.parametrize('comment', [('1')])
+# def test_number(comment):
+#     item = comment
+#     with allure.step(f'Вводные данные: {comment}'):
+#         pass
+#     assert comment == '1'
+#     print(comment)
