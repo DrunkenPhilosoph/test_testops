52dd4b2c64c0930dc6085c7c58d8d726c789d6f4
DrunkenPhilosoph <130548981+DrunkenPhilosoph@users.noreply.github.com>
2023-06-01 17:42:25 +0300

    Update main.py
    asdf

--- main.py
+++ main.py
@@ -41,51 +41,45 @@ def test_invalid_login(username, password):
         # Проверяем, что вход не удался
         pass
 
-# @allure.feature("Моя фича")
-# @allure.story("Мой сценарий")
-# @allure.title("Мой тест")
-# def test_example():
-#     # Шаги теста с декораторами allure
-#     with allure.step("Шаг 1: Подготовка данных"):
-#         # Подготовка данных для теста
-#         data = 'test'
+@allure.feature("Моя фича")
+@allure.story("Мой сценарий")
+@allure.title("Мой тест")
+def test_example():
+    # Шаги теста с декораторами allure
+    with allure.step("Шаг 1: Подготовка данных"):
+        # Подготовка данных для теста
+        data = 'test'
 
-#     with allure.step("Шаг 2: Выполнение действия"):
-#         # Выполнение действия
-#         result = 'test'
+    with allure.step("Шаг 2: Выполнение действия"):
+        # Выполнение действия
+        result = 'test'
 
-#     with allure.step("Шаг 3: Проверка результата"):
-#         # Проверка результата
-#         assert result == data
+    with allure.step("Шаг 3: Проверка результата"):
+        # Проверка результата
+        assert result == data
         
-# @allure.feature("Моя фича")
-# @allure.story("Мой сценарий негативный")
-# @allure.title("Мой тест")
-# def test_example():
-#     # Шаги теста с декораторами allure
-#     with allure.step("Шаг 1: Подготовка данных"):
-#         # Подготовка данных для теста
-#         data = 'test'
+@allure.feature("Моя фича")
+@allure.story("Мой сценарий негативный")
+@allure.title("Мой тест")
+def test_example():
+    # Шаги теста с декораторами allure
+    with allure.step("Шаг 1: Подготовка данных"):
+        # Подготовка данных для теста
+        data = 'test'
 
-#     with allure.step("Шаг 2: Выполнение действия"):
-#         # Выполнение действия
-#         result = 'test'
+    with allure.step("Шаг 2: Выполнение действия"):
+        # Выполнение действия
+        result = 'test'
 
-#     with allure.step("Шаг 3: Проверка результата"):
-#         # Проверка результата
-#         assert result == data
+    with allure.step("Шаг 3: Проверка результата"):
+        # Проверка результата
+        assert result == data
 
-# Запуск тестов с использованием Allure и сохранением отчета
-
-# import allure
-# import pytest
-
-
-# @allure.story('Тестирование метода сравнения чисел')
-# @pytest.mark.parametrize('comment', [('1')])
-# def test_number(comment):
-#     item = comment
-#     with allure.step(f'Вводные данные: {comment}'):
-#         pass
-#     assert comment == '1'
-#     print(comment)
+@allure.story('Тестирование метода сравнения чисел')
+@pytest.mark.parametrize('comment', [('1')])
+def test_number(comment):
+    item = comment
+    with allure.step(f'Вводные данные: {comment}'):
+        pass
+    assert comment == '1'
+    print(comment)
